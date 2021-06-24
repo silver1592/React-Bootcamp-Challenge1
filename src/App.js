130b2585da53e7ae416b360d97cee5e9fc26adb7
Fernando Alvarez Flores <feral1592@gmail.com>
2021-06-23 23:05:29 -0500

    Create Poke card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { PokeCard } from "./components/PokeCard";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Welcome to your first mini challenge!</h1>
+      <PokeCard />
     </div>
   );
 }
